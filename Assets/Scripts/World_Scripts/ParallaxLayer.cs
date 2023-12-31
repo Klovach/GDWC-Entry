e3a9af90344d1aa9f33de0eb46a0bf2500a5bd9b
Markus K <xeinsamkeit@gmail.com>
2023-12-30 17:25:25 -0800

    Revert Change
    Revert Change

--- Assets/Scripts/World_Scripts/ParallaxLayer.cs
+++ Assets/Scripts/World_Scripts/ParallaxLayer.cs
@@ -10,31 +10,9 @@ public class ParallaxLayer : MonoBehaviour
         Vector3 newPos = transform.localPosition;
         newPos.x -= delta * parallaxFactor;
 
-        // Check if the layer is completely out of view on the left & move the layer to the right of all other layers
-        if (newPos.x < -GetLayerWidth())
-        {
-            newPos.x += GetTotalWidth();
-        }
-
         transform.localPosition = newPos;
     }
 
-    float GetLayerWidth()
-    {
-        return GetComponent<Renderer>().bounds.size.x;
-    }
-
-    float GetTotalWidth()
-    {
-        float totalWidth = 0f;
-
-        foreach (ParallaxLayer layer in FindObjectsOfType<ParallaxLayer>())
-        {
-            totalWidth += layer.GetLayerWidth();
-        }
-
-        return totalWidth;
-    }
 }
 
 
